0c2cd5bcd005a3d9276be963f14eededbf5219f5
camilo <camiloramos99@outlook.com.ar>
2024-12-05 04:03:59 -0300

    Add HabitForm component with initial layout and styling

---
+++ src/components/HabitForm/index.jsx
@@ -0,0 +1,51 @@
+const HabitForm = () => {
+    return (
+      <div className="bg-gray-900 rounded-lg shadow-md p-6 max-w-sm mx-auto">
+        
+        <div className="relative">
+            <input 
+                type="date" 
+                className="bg-gray-900 text-gray-100 text-lg font-semibold mb-4 w-full px-4 py-2 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500" 
+                style={{ colorScheme: 'dark' }} 
+            />
+        </div>
+
+        <div className="flex items-center mb-4">
+          <input 
+            type="checkbox" 
+            id="completed" 
+            className="h-4 w-4 text-indigo-400 focus:ring-indigo-500 border-gray-600 rounded" 
+          />
+          <label 
+            htmlFor="completed" 
+            className="ml-2 block text-sm font-medium text-gray-200">
+            Marcar como completado
+          </label>
+        </div>
+  
+        <div className="mb-4">
+          <label 
+            htmlFor="description" 
+            className="block text-sm font-medium text-gray-200">
+            Descripción (opcional)
+          </label>
+          <textarea 
+            id="description" 
+            rows="3" 
+            className="mt-1 p-2 block w-full shadow-sm border border-gray-600 bg-gray-800 text-gray-100 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
+            placeholder="E.g. Series de ejercicio: 3x15"
+          />
+        </div>
+  
+        <div className="flex justify-end">
+          <button className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50">
+            Guardar
+          </button>
+        </div>
+      </div>
+    );
+  };
+    
+  
+  export { HabitForm };
+  
--- src/pages/Habits/index.jsx
+++ src/pages/Habits/index.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
 import { Calendar } from '../../components/Calendar/Calendar';
-
+import { HabitForm } from '../../components/HabitForm/index.jsx';
 
 const Habits = () => {
 
-
     return (
-        <div className='flex items-center flex-col'>
-            <h1>Habits</h1>
-            <div className='w-9/12 border border-black rounded-md p-2' >
-                <Calendar />
-            </div>
-        </div>
-
-    )
+        <>
+            <Calendar />
+            <HabitForm />
+        </>
+    );
 }
 
 export { Habits };
