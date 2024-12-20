939706791c0c6185d6b32bd9a382fddaa8d1cb63
camilo <camiloramos99@outlook.com.ar>
2024-12-20 03:09:46 -0300

    feat: incorporate favicon and logo into site

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,43 +1,33 @@
-import React, { useContext } from "react"
-import { NavBarContext } from "../../contexts/NavbarContext"
+import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
     return (
-        <nav className="flex justify-center space-x-5 bg-white text-sm p-0 border-b-2 "> 
-            <li className="flex p-5 place-self-start text-xl leading-none " >
-                F
-            </li>
-            <ul className="flex w-full place-content-end ">
+        <nav className="flex items-center justify-between bg-white text-sm p-0 border-b-2">
+            <div className="flex items-center p-0 text-xl leading-none">
+                <img 
+                    src="src/assets/logo.png" 
+                    alt="logo" 
+                    className="h-12 w-15 p-0"
+                />
+            </div>
+            <ul className="flex items-center space-x-5 w-full justify-end">
                 <li className="flex p-5 items-center">
                     <Link to="/">Habits</Link>
-                    
                 </li>
-
-                <li className="flex p-5 items-center ">
+                <li className="flex p-5 items-center">
                     <Link to="/to-do">To-DO</Link>
-
-                        
-                    
                 </li>
-
                 <li className="flex p-5 items-center">
                     <Link to="/expense">Expense</Link>
-
-        
                 </li>
-
-                <li className="flex items-center space-x-1 m-0 ml-0	p-5	" >
-                    <button className=" border border-gray-400 bg-gray-300 rounded-md w-16 h-7 " >Sign In</button>
-
-                    <button className="border border-black bg-black rounded-md w-16 h-7 text-white " >Register</button>
+                <li className="flex items-center space-x-1 p-5">
+                    <button className="border border-gray-400 bg-gray-300 rounded-md w-16 h-7">Sign In</button>
+                    <button className="border border-black bg-black rounded-md w-16 h-7 text-white">Register</button>
                 </li>
-
             </ul>
         </nav>
-    )
-
- };
+    );
+};
 
 export { NavBar };
